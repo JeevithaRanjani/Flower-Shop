9ad0b494f8129b2f99476f470a72f8f292361aa6
JeevithaRanjani <71560387+JeevithaRanjani@users.noreply.github.com>
2024-02-04 03:03:12 -0800

    Flower

---
+++ process.php
@@ -0,0 +1,16 @@
+<?php
+include("connection.php");
+if(isset($_POST['but']));
+{
+    $name = $_POST["name"];
+    $email = $_POST["email"];
+    $num = $_POST["num"]; 
+    $result = mysqli_query($con,"Insert into contact values('$name','$email','$num')");
+}
+if($result){
+    echo"Registration Success";
+}
+else{
+echo"Registration Failed";
+} 
+?>
